66d28bba3f008c87f5e22744ee37ae0acbb840d9
Lamia5566 <72219682+Lamia5566@users.noreply.github.com>
2021-04-07 02:05:38 +0800

    Add files via upload

---
+++ Correcting.py
@@ -0,0 +1,101 @@
+import os
+import time
+from datetime import datetime
+from core.tool import tool
+
+
+class auto():
+    def __init__(self, device, NOX=False):
+        try:
+            port = device.split(":")[1]
+        except:
+            port = device
+        self.log = open('log-{0}.txt'.format(port), 'a')
+        self.line = "//================================================\n"
+        self.ship_flag = False
+        self.space_flag = False
+        self.sand_flag = False
+        self.times = 0
+        self.path = os.path.dirname(
+            os.path.dirname(os.path.abspath(__file__))) + "/images"
+        self.adbtool = tool(device, NOX)
+        self.start = None
+        self.end = None
+
+    def check_error(self):
+        error = False
+        error = self.adbtool.compare(
+            ["{0}/error.jpg".format(self.path)], gach=True)
+        while error:
+            self.log_info("連線不佳")
+            self.adbtool.tap((540,1106))
+            error = False
+
+    def log_info(self, str):
+        self.log.write(str)
+        print(str, end='')
+
+    def bot_start(self):
+        print("\033[31mScrpit made by\033[0m \033[41;37mPaver\033[0m,github:\033[37;34mhttps://github.com/Zhen-Bo\033[0m")
+        print(
+            "\033[31m此腳本作者為\033[0m \033[41;37mPaver\033[0m,github頁面:\033[37;34mhttps://github.com/Zhen-Bo\033[0m")
+        os.system('cls')
+        self.log_info(self.line)
+        self.start = datetime.now()
+        self.log_info("開始運行,開始時間: {}\n".format(
+            datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+        print("已鎖定:", end='')
+        i = 0
+        for ark in self.adbtool.ark:
+            print("\033[1;{0}m{1}\033[0m\t".format(
+                33+i, ark.split('.')[0]), end='')
+            i += 1
+        self.log_info("\n{}".format(self.line))
+        POG = []
+        again = False
+        again = self.adbtool.compare(
+            ["{0}/again.jpg".format(self.path)], self.path)
+        try:
+            self.adbtool.tap(again, raw=True)
+        except:
+            raise Exception("沒有偵測到\"再抽一次\",或是解析度不是9x16的解析度\n退出程式")
+        time.sleep(0.5)
+        while len(POG) != len(self.adbtool.ark):
+            t_start = time.time()
+            POG = []
+            again = False
+            self.check_error()
+            while not again:
+                again = self.adbtool.compare(
+                    ["{0}/again.jpg".format(self.path)], gach=True)
+                if not again:
+                    self.check_error()
+                    self.adbtool.tap((960, 70))
+            POG = again[1]
+            t_end = time.time()
+            cost_time = round(t_end-t_start, 2)
+            if self.times == 0:
+                self.times += 1
+            elif self.times != 0 and cost_time > 5:
+                self.log_info("第{0}次結果:\n".format(self.times))
+                if len(POG) > 0:
+                    self.log_info('命中數量: {0}\n'.format(len(POG)))
+                    for i in range(len(POG)):
+                        self.log_info("{0}.{1}\t".format(
+                            i+1, POG[i].split('.')[0]))
+                    self.log_info('\n')
+                else:
+                    self.log_info('命中數量: 0\n')
+                self.log_info("耗時 {0} 秒\n".format(cost_time))
+                self.log_info(self.line)
+                if len(POG) != len(self.adbtool.ark):
+                    self.check_error()
+                    self.adbtool.tap(again[0], raw=True)
+                    time.sleep(1)
+                    self.times += 1
+                elif len(POG) == len(self.adbtool.ark):
+                    self.end = datetime.now()
+                    print(
+                        "執行結束,總共執行 {0} 次, 結束時間 {1} ,共花費 {2} ".format(self.times, self.end, str(self.end-self.start)))
+                    input("請輸入enter繼續")
+                    break
